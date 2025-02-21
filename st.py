49cd18be5a2df60a5b023f0a3c31e50982312e3a
fffxide <fffxide@gmail.com>
2025-02-21 13:37:01 +0900

    change graph title

--- st.py
+++ st.py
@@ -2,6 +2,10 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.font_manager as fm
+
+# 日本語フォントを指定（Windows用）
+plt.rcParams['font.family'] = 'MS Gothic'  # Windows標準のフォント
 
 def summarize_trial_counts_with_ratio(trial_counts, bin_size, max_value):
     bins = np.arange(1, max_value + bin_size, bin_size)
@@ -20,7 +24,7 @@ st.title("試行回数のシミュレーション")
 NUM_SIMULATIONS = st.number_input("シミュレーション回数", value=1000000, step=100000, format="%d")
 X = st.number_input("必要な当選回数", value=5, step=1, format="%d")
 P = st.number_input("1回の試行で当選する確率 (0.01 = 1/100)", value=0.01, step=0.001, format="%.3f")
-BIN_SIZE = st.slider("ヒストグラムのビンサイズ", min_value=100, max_value=2000, value=500, step=100)
+BIN_SIZE = st.slider("ヒストグラムの区分サイズ", min_value=100, max_value=2000, value=500, step=100)
 MAX_VALUE = st.slider("ヒストグラムの最大値", min_value=5000, max_value=50000, value=20000, step=5000)
 
 # シミュレーションの実行
@@ -29,9 +33,9 @@ trial_counts = np.random.negative_binomial(X, P, NUM_SIMULATIONS) + X
 # ヒストグラムの作成
 fig, ax = plt.subplots(figsize=(10, 5))
 ax.hist(trial_counts, bins=50, density=True, alpha=0.6, color='blue', edgecolor='black')
-ax.set_xlabel('試行回数')
-ax.set_ylabel('確率')
-ax.set_title(f'1/{round(1/P, 1)}の抽選で{X}回当選するまでに必要な試行回数の分布')
+ax.set_xlabel('Trials')
+ax.set_ylabel('Probability')
+ax.set_title(f'Distribution of Trials Needed to Win {X} Times in a 1/{round(1/P, 1)} Lottery')
 ax.set_xlim(X, np.percentile(trial_counts, 100))
 
 st.pyplot(fig)
@@ -52,5 +56,5 @@ df_summary = summarize_trial_counts_with_ratio(trial_counts, BIN_SIZE, MAX_VALUE
 st.dataframe(df_summary)
 
 # データのダウンロードリンクを作成
-csv = df_summary.to_csv(index=False).encode("utf-8")
+csv = df_summary.to_csv(index=False).encode("utf-8-sig")
 st.download_button("CSVをダウンロード", data=csv, file_name="simulation_results.csv", mime="text/csv")
